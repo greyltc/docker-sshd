a78bb755fe26b83f44d94339473d9c8253cbda44
Grey Christoforo <grey@christoforo.net>
2016-01-31 18:45:58 +0000

    sshd now allows the root user to login

--- setup-openssh.sh
+++ setup-openssh.sh
@@ -8,3 +8,6 @@ ssh-keygen -A
 
 # allow passwordless (& keyless = no security) ssh access
 sed -i 's,#PermitEmptyPasswords no,PermitEmptyPasswords yes,g' /etc/ssh/sshd_config
+
+# allow the root user to log in
+sed -i 's,#PermitRootLogin prohibit-password,PermitRootLogin yes,g' /etc/ssh/sshd_config
